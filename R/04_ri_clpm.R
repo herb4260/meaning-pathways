fd67b2ed8717813cb593689e5f94f65a27bff28b
herb4260 <herb4260@gmail.com>
2026-08-15 13:10:24 +0900

    Harden longitudinal SEM model inputs

--- R/04_ri_clpm.R
+++ R/04_ri_clpm.R
@@ -1,6 +1,12 @@
 source("R/model_helpers.R")
 dat <- read_longitudinal_data()
 dat <- add_scale_scores(dat)
+
+ri_vars <- c(paste0("mv_t", 1:4), paste0("meaning_t", 1:4))
+ri_dat <- dat[, ri_vars, drop = FALSE]
+ri_dat[] <- lapply(ri_dat, as.numeric)
+stopifnot(!anyDuplicated(names(ri_dat)))
+
 model <- '
   RI_MV =~ 1*mv_t1 + 1*mv_t2 + 1*mv_t3 + 1*mv_t4
   RI_MEAN =~ 1*meaning_t1 + 1*meaning_t2 + 1*meaning_t3 + 1*meaning_t4
@@ -39,6 +45,12 @@ model <- '
   wMV3 ~~ wMEAN3
   wMV4 ~~ wMEAN4
 '
-fit <- sem(model, data = dat, missing = "fiml", estimator = "MLR", meanstructure = TRUE)
+fit <- sem(
+  model,
+  data = ri_dat,
+  missing = "fiml",
+  estimator = "MLR",
+  meanstructure = TRUE
+)
 write.csv(fit_row("meaning_violation_meaning_ri_clpm", fit), "results/ri_clpm_fit.csv", row.names = FALSE)
 write_standardized_paths(fit, "results/ri_clpm_paths.csv")
--- R/06_moderated_mediation.R
+++ R/06_moderated_mediation.R
@@ -1,10 +1,20 @@
 source("R/model_helpers.R")
 dat <- read_longitudinal_data()
 dat <- add_scale_scores(dat)
-for (v in c("mv_t1", "prc_t2", "struggle_t2", "meaning_t3", "recovery_t4", "recovery_t1", "baseline_stressor_severity")) {
-  dat[[paste0("z_", v)]] <- as.numeric(scale(dat[[v]]))
+
+mod_vars <- c(
+  "mv_t1", "prc_t2", "struggle_t2", "meaning_t3",
+  "recovery_t4", "recovery_t1", "baseline_stressor_severity"
+)
+mod_dat <- dat[, mod_vars, drop = FALSE]
+mod_dat[] <- lapply(mod_dat, as.numeric)
+stopifnot(!anyDuplicated(names(mod_dat)))
+
+for (v in mod_vars) {
+  mod_dat[[paste0("z_", v)]] <- as.numeric(scale(mod_dat[[v]]))
 }
-dat$prcXstruggle_t2 <- dat$z_prc_t2 * dat$z_struggle_t2
+mod_dat$prcXstruggle_t2 <- mod_dat$z_prc_t2 * mod_dat$z_struggle_t2
+
 model <- '
   z_prc_t2 ~ a*z_mv_t1
   z_meaning_t3 ~ b*z_prc_t2 + w*z_struggle_t2 + int*prcXstruggle_t2 + m*z_mv_t1
@@ -14,6 +24,14 @@ model <- '
   ind_mean := a*b*c
   ind_high := a*(b + int*(1))*c
 '
-fit <- sem(model, data = dat, missing = "fiml", estimator = "ML", se = "bootstrap", bootstrap = 2000, fixed.x = FALSE)
+fit <- sem(
+  model,
+  data = mod_dat,
+  missing = "fiml",
+  estimator = "ML",
+  se = "bootstrap",
+  bootstrap = 2000,
+  fixed.x = FALSE
+)
 write.csv(fit_row("moderated_serial_mediation", fit), "results/moderated_mediation_fit.csv", row.names = FALSE)
 write_standardized_paths(fit, "results/moderated_mediation_paths.csv")
